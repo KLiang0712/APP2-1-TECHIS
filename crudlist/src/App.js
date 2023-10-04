9623d8973c65d0a30de0e72113783e42d4bba3c5
korliang95 <korliang95@gmail.com>
2023-10-03 20:54:31 -0700

    commit

--- crudlist/src/App.js
+++ crudlist/src/App.js
@@ -1,65 +1,82 @@
-import './App.css';
+import './App.css'; 
 import {useState} from 'react'; 
+import { Task } from './Task'; 
 
 function App() {
-  const [todoList, setTodoList] = useState([]);
-  const [newTask, setNewTask] = useState("");
+
+  /* states */ 
+  const [todoList, setTodoList] = useState([]); // empty array 
+  const [newTask, setNewTask] = useState("");   // empty string 
   
   const handleChange = (event) => {
-    setNewTask(event.target.value)
-  }
+    setNewTask(event.target.value); 
+  };
 
   const addTask = () => {
       const task = {
         id: todoList.length === 0? 1: todoList[todoList.length - 1].id + 1,
         taskName: newTask, 
         completed: false,
-    }
-    setTodoList([...todoList, task])
-  }
+    }; 
+    setTodoList(task.taskName !== " " ? [...todoList, task] : todoList);  
+  };
 
   const deleteTask = (id) => {
-    setTodoList(todoList.filter((task) => task.id !== id)); 
-  }
+    setTodoList(todoList.filter((task) => task.id !== id));
+  }; 
 
   const completeTask = (id) => {
     setTodoList(
       todoList.map((task) => 
       {
           if (task.id === id) {
-            return{...task, completed: true}
+            return{...task, completed: true};
           }
           else
           {
             return task; 
           }
       }) 
-    )
-  }
+    );
+  };
 
   return (
   <div className="App">
       <div className="addTask">  
-        <p id="ptodo">To Do List</p>
+        <h1 id="ptodo">To Do List</h1>
         <input placeholder = "to-do list items" onChange={handleChange}/>
-        <button id="add" onClick={addTask}>Add Task</button>
+        <button id="add" onClick={addTask}>+<br></br>Task</button>
       </div>
 
-          <p id="plist">List your items</p>
+        <h2 id="plist">List your items</h2>
           <div className="list">
-            {todoList.map((task) => {
-                return(
-                  <task taskName = {task.taskName}
-                  id = {task.id}
-                  completed = {task.completed}
-                  deleteTask = {deleteTask} 
-                  completeTask = {completeTask} />
-                ) 
+            {todoList.map((task) => 
+            {
+                return ( 
+                  <Task 
+                    taskName = {task.taskName}
+                    id = {task.id}
+                    completed = {task.completed}
+                    deleteTask = {deleteTask} 
+                    completeTask = {completeTask} 
+                  />
+                );   
             })}
-
           </div>  
-    </div>
-  );
+      </div>
+    );
 }
+
+/*
+  <div className="taskNotes">
+    <h1 id="notes">App Notes</h1>
+        
+    <p>Type a task in the box and then press the "+ Task" button<br></br>
+        to have your task entered.
+    </p>
+
+    <p>Click on the task to either mark it complete or delete it.</p> 
+  </div>
+*/ 
 
 export default App;
