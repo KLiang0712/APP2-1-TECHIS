75263cdfff411eada651635106a934814db39dee
Kory Liang <kory.y.liang@gmail.com>
2024-12-23 22:41:03 -0600

    REACTFILE0712

--- emart/src/redux/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-
-import { cartReducer, selectedProductReducer } from "./handleCart";
-
-export const reducers = combineReducers({
-    allProducts: cartReducer,
-    product : selectedProductReducer,       
-}); 
